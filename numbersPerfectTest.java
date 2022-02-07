1f7b165a2f3c35c847f0782f8937b42bfc006fd8
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2022-02-07 05:52:14 -0500

    Add files via upload

---
+++ numbersPerfectTest.java
@@ -0,0 +1,12 @@
+package logic;
+
+
+import org.junit.jupiter.api.Test;
+
+class numbersPerfectTest {
+    @Test
+    void findPerfect(){
+        System.out.println(numbersPerfect.finPerfects(100,2000));
+    }
+
+}
